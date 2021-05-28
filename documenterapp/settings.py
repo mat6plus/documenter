d4af82fdff322c04052e6f4fd1cdf53b30456c64
mat6plus <mat6plus@gmail.com>
2021-05-28 17:03:54 +0100

    Update

--- documenterapp/settings.py
+++ documenterapp/settings.py
@@ -90,23 +90,23 @@ WSGI_APPLICATION = 'documenterapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'documenter',
-#             'USER': 'adminuser',
-#             'PASSWORD': 'matman020',
-#             'HOST': '127.0.0.1',
-#             'PORT': '5432',
-#         }
-# } 
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': int(os.environ.get('DB_PORT')),
+        }
+} 
 
 
 # if DEBUG:
